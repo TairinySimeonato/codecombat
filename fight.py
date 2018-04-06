3c05932ada9ae89102a869ad8f3f3cfff0c7d9f5
TairinySimeonato <taisimeonato@gmail.com>
2018-04-06 16:46:30 -0300

    Update fight.py

--- fight.py
+++ fight.py
@@ -14,13 +14,13 @@ def find_strongest_and_closest_enemy():
 	strongest_weighted_enemy = None
 
 	for current_enemy in hero.findEnemies():
+		# for slow heros
+		if hero.distanceTo(current_enemy) > 10:
+			continue
+	
 		if strongest_weighted_enemy == None:
 			strongest_weighted_enemy = current_enemy
 			continue
-		
-		# for slow heros
-		if hero.distanceTo(current_enemy) > 30:
-		    continue
 		
 		if enemy_weight(current_enemy) > enemy_weight(strongest_weighted_enemy):
 			strongest_weighted_enemy = current_enemy
